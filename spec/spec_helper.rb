d6beaede4c85d567e27d83d35600b0017db8dad0
Gregg Kellogg <gregg@greggkellogg.net>
2021-08-11 12:31:07 -0700

    Don'e explicitlly use coveralls gem.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,6 @@ require 'rdf/turtle'
 begin
   require 'simplecov'
   require 'simplecov-lcov'
-  require 'coveralls'
 
   SimpleCov::Formatter::LcovFormatter.config do |config|
     #Coveralls is coverage by default/lcov. Send info results
